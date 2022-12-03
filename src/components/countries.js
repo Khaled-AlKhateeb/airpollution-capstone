afbbf7450ae5deca6a92e6f8ca8567d0f362775f
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-12-04 00:53:02 +0300

    fix errors

--- src/components/countries.js
+++ src/components/countries.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Countries = ({
-  countryName, regionName, aqi, image,
-}) => (
-  <button className="country-btn" type="button">
-    <h1 className="country-title">{countryName}</h1>
-    <img src={image} alt={countryName} className="country-flag" />
-    <div className={aqi()}>
-      <h4>Air Quality</h4>
-      <h3 className="quality">{aqi()}</h3>
-    </div>
-    <Link className="link" to={`/${regionName}/${countryName}`}>
-      <div className="link-btn">View Details</div>
-    </Link>
-  </button>
-);
+const Countries = (props) => {
+  const {
+    countryName,
+    regionName,
+    aqi,
+    image,
+  } = props;
 
-Countries.propTypes = {
-  countryName: PropTypes.string.isRequired,
-  regionName: PropTypes.string.isRequired,
-  aqi: PropTypes.func.isRequired,
-  image: PropTypes.string.isRequired,
+    <button className="country-btn" type="button">
+      <h1 className="country-title">{countryName}</h1>
+      <img src={image} alt={countryName} className="country-flag" />
+      <div className={aqi()}>
+        <h4>Air Quality</h4>
+        <h3 className="quality">{aqi()}</h3>
+      </div>
+      <Link className="link" to={`/${regionName}/${countryName}`}>
+        <div className="link-btn">View Details</div>
+      </Link>
+    </button>;
 };
 
 export default Countries;
